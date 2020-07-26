f8d4ebc2b9cbc6d630ad3c9f89c2000715addf63
tiagocarmo <contato@tiagocarmo.com.br>
2020-07-26 16:58:44 -0300

    Updates

---
+++ precache-manifest.0d9e599948733678abf594d986b5d19d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7be4f425a14e8b9c5d6f68803ca43e4",
+    "url": "/react-spa/index.html"
+  },
+  {
+    "revision": "0cf211bda5f31e994a3b",
+    "url": "/react-spa/static/js/2.b8ecfa3a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-spa/static/js/2.b8ecfa3a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b83972e066f4f3078cb",
+    "url": "/react-spa/static/js/main.765624d8.chunk.js"
+  },
+  {
+    "revision": "6aee4666e78f5cf5e621",
+    "url": "/react-spa/static/js/runtime-main.a441180d.js"
+  }
+]);
